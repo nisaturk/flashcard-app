89affc9aeb60ebd81952b355c27927cb2636430f
nisaturk <nisatuurk@gmail.com>
2024-11-04 01:03:28 +0300

    Added CSS for card flip, still needs improvement

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -4,9 +4,26 @@ function Flashcard({ flashcard }) {
     const [flip, setFlip] = useState(false);
 
     return (
-      <div onClick={() => setFlip(!flip)}> {/* Correctly call setFlip to update the state */}
-        {flip ? flashcard.answer : flashcard.question}
-      </div>
+        <div 
+            className={`card ${flip ? 'flip' : ''}`} 
+            onClick={() => setFlip(!flip)}
+        >
+            <div className='card-front'>
+                <div className='flashcard-question'>
+                    {flashcard.question}
+                </div>
+                <div className='flashcard-options'>
+                    {flashcard.options.map(option => (
+                        <div className='flashcard-option' key={option}>
+                            {option}
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className='card-back'>
+                {flashcard.answer}
+            </div>
+        </div>
     );
 }
 
